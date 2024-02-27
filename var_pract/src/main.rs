fb576f0c3ddc3fbfea3367d5865155238d19effd
Chhavi Gupta <chhavigupta1705@gmail.com>
2024-02-28 01:16:26 +0530

    var

---
+++ var_pract/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    // let a = 45; //immutable variable
+    let mut a = 45; //mutable variable
+    println!("The value of a is: {}", a);
+    a = 60;
+    println!("The value of a is: {}", a);
+    let b ="Hello, world!";
+    println!("The value of b is: {}", b);
+
+}
+//  The value of a is: 45
